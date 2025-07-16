53f695fd48e805d04e807ac0d32c8e1a5be3baea
zilin <276161014@qq.com>
2025-07-16 18:30:53 +0800

    feat: 增加环境信息输出和 CDN 图片配置

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
+import { getEnvironmentInfo } from "./lib/env-utils";
+
+// 获取环境信息
+const envInfo = getEnvironmentInfo();
+
+console.log('🔧 Next.js 环境配置:');
+console.log('- 环境:', envInfo.nodeEnv);
+console.log('- 工作模式:', envInfo.workingMode);
+console.log('- Asset Prefix:', envInfo.assetPrefix);
+
+const cdnPrefix = process.env.CLOUDFLARE_PUBLIC_PREFIX;
+const shouldUseCDN = cdnPrefix && cdnPrefix.startsWith('https://');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  // 配置图片域名
+  images: {
+    // 只有在启用 CDN 时才配置远程图片域名
+    remotePatterns: shouldUseCDN ? [
+      {
+        protocol: 'https',
+        hostname: new URL(cdnPrefix).hostname,
+        port: '',
+        pathname: '/**',
+      }
+    ] : [],
+  },
+
 };
 
 export default nextConfig;
