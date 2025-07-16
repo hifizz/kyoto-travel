b9899be9cf227fa29586c92e70c9d99929b6d5a0
zilin <276161014@qq.com>
2025-07-16 17:20:10 +0800

    test: next config

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,7 @@
 import type { NextConfig } from "next";
-import { getEnvironmentInfo } from "./lib/env-utils";
-
-// 获取环境信息
-const envInfo = getEnvironmentInfo();
-
-console.log('🔧 Next.js 环境配置:');
-console.log('- 环境:', envInfo.nodeEnv);
-console.log('- 工作模式:', envInfo.workingMode);
-console.log('- Asset Prefix:', envInfo.assetPrefix);
-
-const cdnPrefix = process.env.CLOUDFLARE_PUBLIC_PREFIX;
-const shouldUseCDN = cdnPrefix && cdnPrefix.startsWith('https://');
 
 const nextConfig: NextConfig = {
-
-  // 配置图片域名
-  images: {
-    // 只有在启用 CDN 时才配置远程图片域名
-    remotePatterns: shouldUseCDN ? [
-      {
-        protocol: 'https',
-        hostname: new URL(cdnPrefix).hostname,
-        port: '',
-        pathname: '/**',
-      }
-    ] : [],
-  },
-
+  /* config options here */
 };
 
 export default nextConfig;
